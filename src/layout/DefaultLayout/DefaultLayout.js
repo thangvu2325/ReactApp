fd148f495dfbaf20ee65eb14fa45180891a890f9
thangvu2325 <thangvu2325@gmail.com>
2023-03-07 22:56:28 +0700

    basic

---
+++ src/layout/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,11 @@
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+// Layouts
+
+// Pages
+import Home from '~/page/Home';
+
+// Public routes
+const publicRoutes = [{ path: '/', component: Home }];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
